9bc9d3e14cc81a2cd8a8eb802e8cce135966738d
Boonie Attawut <puifai.ba@gmail.com>
2020-12-16 03:55:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,14 +3,14 @@ def new_hash
 end
 
 def my_hash
-  :brand => "Milo"
+  {:brand => "Milo"}
 end
 
 def pioneer
-  :name => "Grace Hopper"
+  {:name => "Grace Hopper"}
 end
 
 
 def id_hash_generator(number)
-  :id => number
+  {:id => number}
 end
